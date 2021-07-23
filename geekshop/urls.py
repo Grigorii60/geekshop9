35c8886bb6ca5541b8f73ce2d71f68d954f46725
Frundin Grigorii <fgrigoriy1@yandex.ru>
2021-07-23 12:38:15 +0300

    install debug

--- geekshop/urls.py
+++ geekshop/urls.py
@@ -24,6 +24,6 @@ if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
-   import debug_toolbar
+    import debug_toolbar
 
-   urlpatterns += [re_path(r'^__debug__/', include(debug_toolbar.urls))]
+    urlpatterns += [re_path(r'^__debug__/', include(debug_toolbar.urls))]
